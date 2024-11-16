db1547ef2eed4f12c4e2713dbd6a8420ff7699e7
prince <princepjng85@gmail.com>
2024-11-17 01:01:26 +0530

    api url changed

--- beautySalonFrontend/src/config/config.js
+++ beautySalonFrontend/src/config/config.js
@@ -1,3 +1,6 @@
-const apiUrl = import.meta.env.MODE === "development" ? import.meta.env.VITE_LOCAL_BASE_URL: import.meta.env.VITE_LIVE_BASE_URL;
-console.log(import.meta.env.MODE)
+const apiUrl = `https://salon-booking-services.onrender.com`
+
+
+// import.meta.env.MODE === "development" ? import.meta.env.VITE_LOCAL_BASE_URL: import.meta.env.VITE_LIVE_BASE_URL;
+// console.log(import.meta.env.MODE)
 export default apiUrl;
