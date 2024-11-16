27bbc63364843a5640f08ae5ab0e98bd72b1bd28
prince <princepjng85@gmail.com>
2024-11-16 22:24:28 +0530

    env file update

--- beautySalonFrontend/src/config/config.js
+++ beautySalonFrontend/src/config/config.js
@@ -1,3 +1,3 @@
-const apiUrl = import.meta.env.MODE === "development" ? import.meta.env.VITE_LIVE_BASE_URL : import.meta.env.VITE_LOCAL_BASE_URL;
+const apiUrl = import.meta.env.MODE === "development" ? import.meta.env.VITE_LOCAL_BASE_URL: import.meta.env.VITE_LIVE_BASE_URL;
 
 export default apiUrl;
