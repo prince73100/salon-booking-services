64c2a2e56dc0c1ae892a58b70191eaabef6cc1f4
prince <princepjng85@gmail.com>
2024-12-02 17:13:19 +0530

    readme fle add

--- beautySalonFrontend/README.md
+++ beautySalonFrontend/README.md
@@ -1,8 +1,195 @@
-# React + Vite
+# Project: Online Salon Appointment Booking App 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Abstract:
 
-Currently, two official plugins are available:
+The  "Online  Salon  Appointment  Booking  App"  is  a  comprehensive  application designed to streamline the salon appointment booking process. This app provides a user-friendly platform for customers to book appointments online, view available time slots, and receive confirmations. The system also benefits salon owners by efficiently managing  appointments,  reducing  no-shows,  and  enhancing  overall  customer experience. 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Problem Statement
+
+- **Salon Market is very unorganized in India.. almost 90-95% salon Market in India is not connected with technology** 
+
+  The Indian salon industry has seen impressive growth over the past few years, with the number of salons and beauty and personal care service providers increasing rapidly. According to a recent report, the Indian salon industry was valued at approximately INR 90,000 crore in 2020 and is expected to reach INR 1.75 trillion by 2026, growing at a CAGR of 15% from 2021 to 2026. This growth is being driven by many factors, including the increasing popularity of beauty and personal care services, the growth of the e-commerce sector, and the growing use of social media to promote beauty and personal care products and services.  
+
+# Project Methodology 
+
+Creating a full-fledged online booking app for salons using React JS, Express, Node.js, and MongoDB is a complex project that involves multiple components.  
+
+1. **Project Setup:** 
+- Set  up  a  new  ReactJS  project  via  react  vite for  the  client-side  of  your application. 
+- Create a new Express.js project for the server-side. 
+- Set up a MongoDB database to store salon and appointment data. 
+2. **User Authentication:** 
+- Implement user authentication for customers, salon owners. 
+- Use technologies like JWT (JSON Web Token) for authentication. 
+
+**4. Booking System:** 
+
+- Create a booking system where customers can view available time slots and book appointments. 
+- Implement a calendar for salons to manage their schedules. 
+
+**7. Ratings and Reviews:** 
+
+- Allow customers to leave ratings and reviews for salons  
+- Display these ratings to help customers make informed decisions. 
+
+**10. Dashboard for Business Owners:** 
+
+- Create dashboards for salon owners and customers to manage appointments, view earnings, and monitor customer feedback. 
+12. **Testing and Quality Assurance:** 
+- Ensure cross-browser and cross-device compatibility. 
+13. **Deployment:** 
+- Deploy the React jS frontend and Express backend to hosting services. 
+
+# 1.Web App for Salon
+
+If a salon is registered and logged in, it can perform the following actions: 
+
+1. Add products to its salon. 
+1. Delete products. 
+1. Edit product details (only the price). 
+1. Post job openings to hire artists. 
+1. View and edit its own profile." 
+
+![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.001.jpeg)
+
+If a salon is logged in and it is not register, then it can perform the following actions: 1.Only it Access registered salon .
+
+![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.002.jpeg)
+
+- A Registration Page for Salon where salon can register itself. input fields will be Salon      Name, Salon Type, Address, email id, contact number and set Password 
+
+and confirm password.
+
+![ref1]
+
+**(Sign up page)** 
+
+![ref2]
+
+**(Sign In page)** 
+
+- salon can add list of services which will be shown to customers 
+
+  ![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.005.jpeg)
+
+- Salon can accept or cancel Booking slot requested by customer  
+
+  ![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.006.jpeg)
+
+**(Not yet functioning)** 
+
+- salon can upload timing will be shown to customer 
+- Salon can upload salon photos and videos and work and customers can see. 
+
+# 2.Web App for Customer 
+
+- Customers can register themselves with information like Name, Address, contact number, email id, gender and Set Password. 
+
+![ref1]
+
+**(Sign up page)** 
+
+![ref2]
+
+**(Sign In page)** 
+
+- Can search Salon with given range 
+
+![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.007.jpeg)
+
+- Can give rating to Salon based on experience in Salon 
+
+![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.008.jpeg)
+
+- can request for Quote to Salon for service going to Avail   
+- can book appointment with timeslot and date based on salon/Artist availability  
+
+  ![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.009.jpeg)
+
+- Compare salon and Price. 
+
+  ![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.010.jpeg)
+
+- Can payment via two method(Cash less or with Cash) 
+
+![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.011.jpeg)
+
+- Can Access own Booking Histroy. 
+
+![](public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.012.jpeg)
+
+# Summary of this project:
+
+This project features a two-panel architecture designed to cater to two user roles: Users and Salons. 
+
+**User Panel:** 
+
+- **Role:** Users can browse salons and services, book appointments, and provide feedback. 
+- **Features:** 
+1. Salon Booking: Users can browse and book services offered by nearby salons. 
+1. Ratings and Reviews: Users can leave comments and rate salons based on their experience. 
+1. Appointment Management: Users can view and manage their booked appointments. 
+
+**Salon Panel:** 
+
+- **Role**: Salons can manage their services and job postings after proper registration. 
+- **Features**: 
+1. **Conditional Access**: 
+   1. If logged in but not registered, salons cannot perform CRUD operations or access additional features. 
+   1. Full functionality is available only after login and registration. 
+1. **Service Management**: Add, edit (price only), or delete services. 
+1. **Job Posting**: Post job openings to hire professionals. 
+1. **Booking Management**: View upcoming bookings from users. 
+
+# Link:
+
+## Repository:
+
+**Frontend:   [ https://github.com/prince73100/salon-booking- services/tree/main/beautySalonFrontend** ](https://github.com/prince73100/salon-booking-services/tree/main/beautySalonFrontend)**
+
+**Backend :  [** https://github.com/prince73100/salon-booking- services/tree/main/beautySalonBackend ](https://github.com/prince73100/salon-booking-services/tree/main/beautySalonBackend)**
+
+**Deployment Link:[** https://beautytime-eta.vercel.app/ ](https://beautytime-eta.vercel.app/)
+
+
+# Command:
+
+## React + Vite:
+
+`    `**Start**:  npm run dev 
+
+`    `**Build**:  npm run build  
+
+## Node Server:
+
+`  `**Start:** npm run start 
+
+
+
+# Instructions:
+
+**Note**:  After clicking on the link, please wait for a few moments for the backend server to start. This is because the application is hosted on a free platform, which may cause a slight 
+delay in initialization. 
+
+# For access:
+
+### Login for user:
+
+  Email id: testuser1@gmail.com 
+
+  Password: test123** 
+
+
+### Login for Salon:
+
+#### This is for registered salon:
+
+ Email id: arvindsalon@gmail.com    
+ Password: test123 
+
+`  `**If salon user is not register salon:** 
+
+`  `Email id: testsalon5@gmail.com    Password: test123 
+
+[ref1]: public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.003.jpeg
+[ref2]: public/Aspose.Words.6c84407c-4266-4d8d-92d2-b040cddbe491.004.jpeg
--- beautySalonFrontend/src/component/bookbytheservice/Bookinghistory.jsx
+++ beautySalonFrontend/src/component/bookbytheservice/Bookinghistory.jsx
@@ -1,40 +1,43 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import * as React from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
-import apiUrl from '../../config/config';
+import * as React from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import apiUrl from "../../config/config";
 export default function BasicTable() {
   const [bookDetail, setBookingDetail] = React.useState([]);
-  const token = localStorage.getItem('jwt');
+  const token = localStorage.getItem("jwt");
 
   const fetchAllBookingHistory = async () => {
-    const result = await axios.get(`${apiUrl}/api/v1/booked/getbookingHistory`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const result = await axios.get(
+      `${apiUrl}/api/v1/booked/getbookingHistory`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     setBookingDetail(result.data.bookHistory);
     console.log(result);
   };
-
 
   useEffect(() => {
     fetchAllBookingHistory();
   }, []);
 
   return (
-    <div className=' '>
+    <div className=" ">
       <div className="hidden sm:block sm:w-full sm:flex sm:justify-center sm:mt-20">
         <div className="w-11/12">
           <h1 className="bg-rose-700 text-center  text-2xl font-bold py-5 text-white">
@@ -63,15 +66,35 @@ export default function BasicTable() {
               </TableHead>
               <TableBody>
                 {bookDetail?.map((item) => (
-                  <TableRow key={item._id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableRow
+                    key={item._id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
                     <TableCell component="th" scope="row">
                       {item.serviceName}
                     </TableCell>
                     <TableCell align="right">{item.price}</TableCell>
-                    <TableCell align="right">{item.ispayment ? <span className='text-green-500'>Online pament</span>: <span className='text-rose-500'>Cash payment</span>}</TableCell>
-                    <TableCell align="right">{item.salonID.salonName}</TableCell>
                     <TableCell align="right">
-                      {item.serviceDateAndTime?.split('T')[0]} | {item?.serviceDateAndTime?.split('T')[1]?.slice(0, 5)} <span>{item.serviceDateAndTime?.split('T')[1]?.slice(0, 5)?.split(':')[0]>= 12?'PM' :'AM'}</span>
+                      {item.ispayment ? (
+                        <span className="text-green-500">Online pament</span>
+                      ) : (
+                        <span className="text-rose-500">Cash payment</span>
+                      )}
+                    </TableCell>
+                    <TableCell align="right">
+                      {item.salonID.salonName}
+                    </TableCell>
+                    <TableCell align="right">
+                      {item.serviceDateAndTime?.split("T")[0]} |{" "}
+                      {item?.serviceDateAndTime?.split("T")[1]?.slice(0, 5)}{" "}
+                      <span>
+                        {item.serviceDateAndTime
+                          ?.split("T")[1]
+                          ?.slice(0, 5)
+                          ?.split(":")[0] >= 12
+                          ? "PM"
+                          : "AM"}
+                      </span>
                     </TableCell>
                   </TableRow>
                 ))}
@@ -81,16 +104,16 @@ export default function BasicTable() {
         </div>
       </div>
 
-      <div className='sm:hidden block mt-20'>
+      <div className="sm:hidden block mt-20">
         <h1 className="bg-rose-700 text-center  text-2xl font-bold py-5 text-white">
           Booking History
         </h1>
         <div className=" w-full h-full flex justify-center ">
           <div className="w-11/12  flex justify-center">
-            <List sx={{ width: '90%', maxWidth: 360 }}>
+            <List sx={{ width: "90%", maxWidth: 360 }}>
               {bookDetail.map((item) => (
-                <div className='border-b-2 border-rose-500' key={item._id}>
-                  <ListItem alignItems="flex-start" >
+                <div className="border-b-2 border-rose-500" key={item._id}>
+                  <ListItem alignItems="flex-start">
                     <ListItemText
                       primary={item?.salonID?.salonName}
                       secondary={
@@ -98,20 +121,55 @@ export default function BasicTable() {
                           <Typography
                             component="span"
                             variant="body2"
-                            sx={{ color: 'text.primary', display: 'inline' }}
+                            sx={{ color: "text.primary", display: "inline" }}
                           >
-                            <span className='text-base font-semibold text-rose-600'>{item?.serviceName}- </span>
+                            <span className="text-base font-semibold text-rose-600">
+                              {item?.serviceName}-{" "}
+                            </span>
                           </Typography>
-                          <span className='text-lg font-semibold'><span className='pl-5'>&#8377;</span>{` ${item?.price}`}</span> <br />
-                          <span className=' '><span className='text-rose-600 font-semibold'>Date and Time - </span> {item.serviceDateAndTime?.split('T')[0]} | {item.serviceDateAndTime?.split('T')[1]?.slice(0, 5)}</span> <span>{item.serviceDateAndTime?.split('T')[1]?.slice(0, 5)?.split(':')[0]>= 12?'PM' :'AM'}</span> <br />
-                          <span className='text-sm  font-bold text-rose-600'>Payment Method: <span>{item.ispayment ? <span className='text-green-700'>Online pament</span>: <span className='text-black '>Cash payment</span>}</span> </span>
+                          <span className="text-lg font-semibold">
+                            <span className="pl-5">&#8377;</span>
+                            {` ${item?.price}`}
+                          </span>{" "}
+                          <br />
+                          <span className=" ">
+                            <span className="text-rose-600 font-semibold">
+                              Date and Time -{" "}
+                            </span>{" "}
+                            {item.serviceDateAndTime?.split("T")[0]} |{" "}
+                            {item.serviceDateAndTime
+                              ?.split("T")[1]
+                              ?.slice(0, 5)}
+                          </span>{" "}
+                          <span>
+                            {item.serviceDateAndTime
+                              ?.split("T")[1]
+                              ?.slice(0, 5)
+                              ?.split(":")[0] >= 12
+                              ? "PM"
+                              : "AM"}
+                          </span>{" "}
+                          <br />
+                          <span className="text-sm  font-bold text-rose-600">
+                            Payment Method:{" "}
+                            <span>
+                              {item.ispayment ? (
+                                <span className="text-green-700">
+                                  Online pament
+                                </span>
+                              ) : (
+                                <span className="text-black ">
+                                  Cash payment
+                                </span>
+                              )}
+                            </span>{" "}
+                          </span>
                         </React.Fragment>
                       }
                     />
                   </ListItem>
                 </div>
               ))}
-
             </List>
           </div>
         </div>
